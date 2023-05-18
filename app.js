577c80cb3023bd34525ec22556be3cf2db4a4e32
RinaldyD <darmawan.rinaldy@gmail.com>
2023-05-18 15:30:04 +0700

    Revise and adding Controller, Middleware, and dotenv

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const router = require('./routes')
+const router = require('./routes/routes')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
