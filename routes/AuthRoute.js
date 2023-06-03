7a83aa3166422c04698a2cc59a1c095b3c766790
Rozan Maulana <rozanmln.wp@gmail.com>
2023-06-03 10:54:02 +0700

    struktur ulang file MD

--- routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const { loginPatient, loginHospital } = require("../controllers/Auth")
-
-const router = express.Router()
-
-router.post('/loginPatient', loginPatient)
-router.post('/loginHospital', loginHospital)
-
-module.exports = router
