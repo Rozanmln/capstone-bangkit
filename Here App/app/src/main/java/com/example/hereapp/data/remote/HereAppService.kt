7e501695c62ba105d86c91f0091f1ff1c2380db8
Rozan Maulana <rozanmln.wp@gmail.com>
2023-06-03 10:43:23 +0700

    revert cc branch

---
+++ Here App/app/src/main/java/com/example/hereapp/data/remote/HereAppService.kt
@@ -0,0 +1,20 @@
+package com.example.hereapp.data.remote
+
+import com.example.hereapp.data.model.RegisterHospitalRequest
+import com.example.hereapp.data.model.RegisterHospitalResponse
+import com.example.hereapp.data.model.RegisterPatientRequest
+import com.example.hereapp.data.model.RegisterPatientResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface HereAppService {
+    @POST("hospital")
+    suspend fun postRegisterHospital(
+        @Body request: RegisterHospitalRequest
+    ): RegisterHospitalResponse
+
+    @POST("patient")
+    suspend fun postRegisterPatient(
+        @Body request: RegisterPatientRequest
+    ): RegisterPatientResponse
+}
