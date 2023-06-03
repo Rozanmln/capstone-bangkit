1f760bbb03d8f42ce9819dd906c42a6ede30aa5c
Rozan Maulana <rozanmln.wp@gmail.com>
2023-06-03 10:14:48 +0700

    delete ML and hereapp MD

--- Here App/app/src/main/java/com/example/hereapp/data/repository/HereRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hereapp.data.repository
-
-import com.example.hereapp.data.preferences.UserPreferences
-import com.example.hereapp.data.remote.HereAppService
-
-class HereRepository(private val pref: UserPreferences, private val apiService: HereAppService) {
-
-}
