18af5c7b7d44cef73bce7f8e0bd13379e6d6df24
RinaldyD <darmawan.rinaldy@gmail.com>
2023-06-14 13:28:30 +0700

    revise flow patient

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const { loginPatient, loginHospital, logout } = require("../controllers/Auth")
+const express = require('express');
+const { loginPatient, loginHospital, logout } = require('../controllers/Auth');
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/loginPatient', loginPatient)
-router.post('/loginHospital', loginHospital)
-router.post('/logout', logout)
+router.post('/loginPatient', loginPatient);
+router.post('/loginHospital', loginHospital);
+router.post('/logout', logout);
 
-module.exports = router
+module.exports = router;
