4de7231ad7bdfc5f4521f1c203f5975d8f465fe3
RinaldyD <darmawan.rinaldy@gmail.com>
2023-06-16 18:02:08 +0700

    replace database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('here2', 'root', 'password', {
-  host: '34.101.119.140',
+const db = new Sequelize('here2', 'root', 'capstoneC23', {
+  host: '35.193.38.255',
   dialect: 'mysql',
 });
 
