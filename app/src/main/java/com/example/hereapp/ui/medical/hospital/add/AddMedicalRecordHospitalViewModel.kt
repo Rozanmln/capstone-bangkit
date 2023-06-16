443e1754daf560aa8cd5daf9a8d3765b3f66aaf6
RinaldyD <darmawan.rinaldy@gmail.com>
2023-06-16 21:31:26 +0700

    restructure main branch

--- app/src/main/java/com/example/hereapp/ui/medical/hospital/add/AddMedicalRecordHospitalViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hereapp.ui.medical.hospital.add
-
-import androidx.lifecycle.ViewModel
-import com.example.hereapp.data.model.MedicalRecordRequest
-import com.example.hereapp.data.repository.HereRepository
-
-class AddMedicalRecordHospitalViewModel(private val hereRepository: HereRepository): ViewModel() {
-    fun postCreateMedicalRecord(request: MedicalRecordRequest) = hereRepository.postCreateMedicalRecord(request)
-}
