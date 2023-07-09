3f2eced9e938e76169a68f8da89377989d941c62
RinaldyD <darmawan.rinaldy@gmail.com>
2023-07-09 15:14:31 +0700

    revised env

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('here2', 'root', 'capstoneC23', {
-  host: '35.193.38.255',
-  dialect: 'mysql',
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
 
 module.exports = db;
